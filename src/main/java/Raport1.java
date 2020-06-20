aa0fbb458c6213157489cbeb7a0979b290b7bf35
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-06-20 14:43:08 +0200

    Raport1 implemented

--- src/main/java/Raport1.java
+++ src/main/java/Raport1.java
@@ -1,16 +1,31 @@
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Raport1 implements Raport {
 
 	@Override
 	public String[][] generateRaport(Set<Project> projects) {
+		Set<Employee> employees = parseEmployees(projects);
 		
-		String[][] raport = new String[][];
+		String[][] raport = new String[employees.size()][2];
 		
-		// Jak starczy czasu, to zrobić posortowaną tablicę
-
-		
+		int i = 0;
+		for (Employee employee : employees) {
+			raport[i][0] = employee.getName();
+			int workedHours = 0;
+			
+			for (Task task : employee.getTasks()) {
+				workedHours += task.getDuration();
+			}
+			
+			raport[i][1] = String.valueOf(workedHours);
+			i++;
+		}
+				
+		// Docelowo posortować
+		return raport;		
 	}
 	
 	private Set<Employee> parseEmployees(Set<Project> projects) {
@@ -21,5 +36,7 @@ public class Raport1 implements Raport {
 				employees.add(employee);
 			}
 		}
+		
+		return employees;
 	}
 }
