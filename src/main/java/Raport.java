fb2b158a1d3440a5b3da41cc3985f6128136c31f
Marcin <mieker@wp.pl>
2020-06-20 21:45:44 +0200

    branch DataRaportPrinter ready to send

--- src/main/java/Raport.java
+++ src/main/java/Raport.java
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-
-public class Raport {
-
-    String raport[][] = { { "Pracownik", "Roboczogodziny" }, { "Jan Kowalski", "12" }, { "Piotr Nowak", "20" }, };
-    String name = "Raport nr 1";
-
-    public String[][] getRaport() {
-        return raport;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-       
-
-=======
 import java.util.Set;
 
 public interface Raport {
@@ -23,5 +5,4 @@ public interface Raport {
 	public String[][] generateRaport(Set<Project> projects);
 	public String[][] getRaport();
 	public String getName();
->>>>>>> d6d3142ec72ea76210d744b304b089a42b9d6915
 }
