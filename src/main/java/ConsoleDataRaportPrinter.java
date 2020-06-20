9a06685794b13428a157925aff92a0afa9fb47e7
Mieker <60047226+Mieker@users.noreply.github.com>
2020-06-20 16:02:47 +0200

    DataRaportPrinter interface and Console printer

---
+++ src/main/java/ConsoleDataRaportPrinter.java
@@ -0,0 +1,18 @@
+
+public class ConsoleDataRaportPrinter implements DataRaportPrinter {
+
+    @Override
+    public void printRaport(Raport raport) {
+        
+        System.out.println("Raport: " + raport.getName());
+        for (String[] record : raport.getRaport()) {
+            for (String value : record) {
+                String line = String.format("%15s", value);
+                System.out.print(line);
+            }
+            System.out.println();
+        }
+    }
+
+}
+
