ae166096cae252d669d35ead82fcafc0fd6b05c6
Lukasz Kaczmarek <lukaszk76@gmail.com>
2020-06-20 14:32:23 +0200

    scanFolder ready

--- src/main/java/DataImporter.java
+++ src/main/java/DataImporter.java
@@ -1,15 +1,40 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.Set;
+import java.nio.file.Files;
 
 public class DataImporter {
 
-	Set<Project> private getTasks(ArrayList<String> files) {
-		//TO DO
+	private ArrayList<String> paths = new ArrayList<String>();
+
+	private Set<Project> getTasks(ArrayList<String> files) {
+		// TO DO
 	}
-	
-	ArrayList<String> private scanFolder(String path) {
-		//TO DO
+
+	private void scanFolder(String path) {
+
+		File folder = new File(path);
+		File[] listOfFiles = folder.listFiles();
+		for (File file : listOfFiles) {
+			try {
+				BasicFileAttributes attributes = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+				System.out.println(file.getPath());
+				if (attributes.isDirectory()) {
+					scanFolder(file.getPath());
+				} else {
+					this.paths.add(file.getPath());
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
 	}
-	
-	Set<Project> public importDataFromFiles(String path) {
-		return getTasks(scanFolder(path));
+
+	public Set<Project> importDataFromFiles(String path) {
+		scanFolder(path);
+		return getTasks(this.paths);
 	}
 }
