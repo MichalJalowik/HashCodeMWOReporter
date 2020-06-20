be5118cde73d880308f9e87e195ca64a474530ba
MJ <michaljalowik@gmail.com>
2020-06-20 21:03:31 +0200

    dodana funkcja odpalania z wiersza polecen

--- src/main/java/MainJarAndCommandLineTEST.java
+++ src/main/java/MainJarAndCommandLineTEST.java
@@ -3,41 +3,67 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+import java.io.IOException;
+import java.util.Set;
+
 public class MainJarAndCommandLineTEST {
 
 
     public static void main (String[] args) throws ParseException{
 
+        DataImporter importer = new DataImporter();
+
         Options options = new Options();
         options.addOption("path", true, "put path of location");
-        options.addOption("report", true, "put report needed");
-        options.addOption("printer", true, "put printer type");
+        options.addOption("report1", false, "put report needed");
+        options.addOption("report2", false, "put printer type");
 
         CommandLineParser parser = new DefaultParser();
         try {
             CommandLine line = parser.parse( options, args );
             String path = line.getOptionValue("path");
-            String report = line.getOptionValue("report");
-            String printer = line.getOptionValue("printer");
+            String report1 = line.getOptionValue("report");
+            String report2 = line.getOptionValue("printer");
 
             if(line.hasOption("path")) {
-                System.out.println("running......path" + " " + path);
+                //System.out.println("running......path" + " " + path);
+                //Set<Project> set = importer.importDataFromFiles(path);
             }
-            if(line.hasOption("report")) {
-                System.out.println("running......report" + " " + report);
+            if(line.hasOption("report1")) {
+                //System.out.println("running......report1" + " " + report1);
+
+                Set<Project> set = importer.importDataFromFiles(path);
+                Raport raport1 = new Raport1();
+                raport1.generateRaport(set);
+                DataRaportPrinter print1 = new ConsoleDataRaportPrinter();
+                try {
+                    print1.printRaport(raport1);
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+
             }
-            if(line.hasOption("printer")) {
-                System.out.println("running......printer" + " " + printer);
+            if(line.hasOption("report2")) {
+                //System.out.println("running......report2" + " " + report2);
+
+                Set<Project> set = importer.importDataFromFiles(path);
+                Raport2 raport2 = new Raport2();
+                raport2.generateRaport(set);
+                DataRaportPrinter print2 = new ConsoleDataRaportPrinter();
+                try {
+                    print2.printRaport(raport2);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-            else {
-                System.out.println("no function to run");
-            }
+//            else {
+//                System.out.println("no function to run");
+//            }
         }
         catch( ParseException exp ) {
             System.err.println( "Parsing failed.  Reason: " + exp.getMessage() );
         }
-
-
 
     }
 
