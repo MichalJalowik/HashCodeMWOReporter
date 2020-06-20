aae0c4750b4dcc5025f1757aa2b0ee45d4d64d80
Mieker <60047226+Mieker@users.noreply.github.com>
2020-06-20 21:53:49 +0200

    fixed merge conflict

--- src/main/java/DataRaportPrinter.java
+++ src/main/java/DataRaportPrinter.java
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 import java.io.IOException;
 
 public interface DataRaportPrinter {
 
     public void printRaport(Raport raport) throws IOException;
 }
-=======
-import java.io.IOException;
-
-public interface DataRaportPrinter {
-
-    public void printRaport(Raport raport) throws IOException;
-}
->>>>>>> d6d3142ec72ea76210d744b304b089a42b9d6915
