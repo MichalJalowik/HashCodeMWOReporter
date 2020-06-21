5defb0e9c3e1bb7255aa776d4efd97f0c644a6ef
Lukasz Kaczmarek <lukaszk76@gmail.com>
2020-06-21 21:43:17 +0200

    Improved robust menu in Reporter

--- src/main/java/Reporter.java
+++ src/main/java/Reporter.java
@@ -12,64 +12,70 @@ public class Reporter {
 		String path = scan.nextLine();
 		DataImporter importer = new DataImporter();
 		Set <Project> set = importer.importDataFromFiles(path);
-
-		System.out.println("Wybierz rodzaj raportu, który chcesz wygenerwać:\n "
-				+ "1. Raport godzin przepracowanych przez poszczególnych pracowników\n "
-				+ "2. Raport godzin poświęconych na każdy projekt\n " 
-				+ "3. Raport czasowy pracownik/projekt\n "
-				+ "4. Wyjście z pogramu");
-		int i = scan.nextInt();
+		
+		Menu menu1 = new Menu("WYBÓR RAPORTU", new String[]{
+				"Raport godzin przepracowanych przez poszczególnych pracowników", 
+				"Raport godzin poświęconych na każdy projekt",
+				"Raport czasowy pracownik/projekt"});
+        
+		Menu menu2 = new Menu("WYBÓR PREZENTACJI RAPORTU", new String[]{
+				 "Wyświetlenie na konsoli", 
+				 "Eksport do pliku CSV",
+				 "Eksport do pliku XLSX",
+				 "Eksport do pliku PDF"});
+		
+		
 		Raport raport = null;
-		switch (i) {
-		case 1:
-			raport = new Raport1();
-			
-			break;
-		case 2:
-			raport = new Raport2();
-			break;
-		case 3:
-			raport = new Raport3();
-			break;
-		case 4:
-			break;
-		default:
-			System.out.println("Wybrano niepoprawny numer raportu!");
-		}
-		raport.generateRaport(set);
-		
-		System.out.println("Wybierz sposób prezentacji raportu:\n "
-				+ "1. Wyświetlenie na konsoli\n "
-				+ "2. Eksport do pliku csv\n "
-				+ "3. Eksport do pliku xls\n "
-				+ "4. Eksport do pliku pdf\n " 
-				+ "5. Wyjście z pogramu");
-		i = scan.nextInt();
 		DataRaportPrinter printer = null;
-		switch (i) {
-		case 1:
-			printer = new ConsoleDataRaportPrinter();
-			break;
-		case 2:
-			printer = new CSVDataRaportPrinter();
-			break;
-		case 3:
-			printer = new ExcelDataRaportPrinter();
-			break;
-		case 4:
-			printer = new PDFDataRaportPrinter();
-			break;
-		case 5:
-			break;
-		default:
-			System.out.println("Wybrano niepoprawny numer!");
-		}
 		
-		try {
-			printer.printRaport(raport);
-		} catch (IOException e)
-		{
-			e.printStackTrace();
+		int selection1 = -1;
+		int selection2 = -1;
+		
+		while (selection1 != 0) {
+			selection1 = menu1.makeSelection();
+			switch (selection1) {
+			case 1:
+				raport = new Raport1();
+				break;
+			case 2:
+				raport = new Raport2();
+				break;
+			case 3:
+				raport = new Raport3();
+				break;
+			}
+			raport.generateRaport(set);
+			
+			
+			if (selection1 != 0) {
+				selection2 = -1;
+				while (selection2 != 0) {
+					selection2 = menu2.makeSelection();
+					switch (selection2) {
+					case 1:
+						printer = new ConsoleDataRaportPrinter();
+						break;
+					case 2:
+						printer = new CSVDataRaportPrinter();
+						break;
+					case 3:
+						printer = new ExcelDataRaportPrinter();
+						break;
+					case 4:
+						printer = new PDFDataRaportPrinter();
+						break;
+					}
+					
+					if (selection2 != 0) {
+						try {
+							printer.printRaport(raport);
+						} catch (IOException e)
+						{
+							e.printStackTrace();
+						}
+					}
+				}
+			}
 		}
 	}
 }
