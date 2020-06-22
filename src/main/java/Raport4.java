9dc83ac969c816c4db32d3afb6393f6e025bc8b9
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-06-22 19:58:21 +0200

    Empty Raport4 class created

---
+++ src/main/java/Raport4.java
@@ -0,0 +1,36 @@
+import java.time.LocalDate;
+import java.util.Set;
+
+public class Raport4 implements Raport {
+
+	@Override
+	public String[][] generateRaport(Set<Project> projects) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[][] getRaport() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LocalDate getMinDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LocalDate getMaxDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
