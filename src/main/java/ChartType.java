25ce7a683775663e3969d249ff189bc06f77e0fb
Marcin <mieker@wp.pl>
2020-06-24 18:01:51 +0200

    ChartDataRaportPrinter early implementation ready

---
+++ src/main/java/ChartType.java
@@ -0,0 +1,9 @@
+import org.knowm.xchart.CategoryChart;
+import org.knowm.xchart.demo.charts.ExampleChart;
+
+public interface ChartType extends ExampleChart<CategoryChart> {
+    public CategoryChart getChart();
+    public String getExampleChartName();
+    
+    public void printChart(Raport raport);
+}
