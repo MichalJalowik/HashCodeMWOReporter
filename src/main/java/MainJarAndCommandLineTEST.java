cb82b8602f0e4bad407e1c0398cdbc5384b6c631
MJ <michaljalowik@gmail.com>
2020-06-24 23:39:19 +0200

    added full functionality from command line

--- src/main/java/MainJarAndCommandLineTEST.java
+++ src/main/java/MainJarAndCommandLineTEST.java
@@ -2,12 +2,15 @@ import org.apache.commons.cli.*;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Set;
 public class MainJarAndCommandLineTEST {
     public static void main (String[] args) throws ParseException{
         DataImporter importer = new DataImporter();
         Raport raport = null;
+        Filter filter = new Filter();
         //DataRaportPrinter printer = null;
+        HelpFormatter formatter = new HelpFormatter();
 
         Options options = new Options();
         options.addOption("path", true, "put path of location");
@@ -19,17 +22,39 @@ public class MainJarAndCommandLineTEST {
         options.addOption("CSV", false, "printing result as CSV into project directory");
         options.addOption("XLS", false, "printing result as XLS into project directory");
         options.addOption("PDF", false, "printing result as PDF into project directory");
+        options.addOption("chart", false, "printing result as chart into project directory");
         options.addOption("info", false, "operations manual");
+        options.addOption("filterEMP", true, "filter data by employee name");
+        options.addOption("filterDATE", true, "filter data by date format DD.MM.YYYY_DD.MM.YYYY");
+
+        final PrintWriter writer = new PrintWriter(System.out);
+        formatter.printUsage(writer,80,"MainJarAndCommandLineTEST", options);
+        writer.flush();
+
+
         CommandLineParser parser = new DefaultParser();
         try {
             CommandLine line = parser.parse( options, args );
             String path = line.getOptionValue("path");
+            String filterEMP = line.getOptionValue("filterEMP");
+            String filterDATE = line.getOptionValue("filterDATE");
+
             if(line.hasOption("path")) {
                 System.out.println("running......path" + " " + path);
             }
             if(line.hasOption("report1")) {
                 Set<Project> set = importer.importDataFromFiles(path);
                 raport = new Raport1();
+
+                if(line.hasOption("filterEMP")){
+                    set = filter.filterByEmployeeName(set,filterEMP);
+                }
+                if(line.hasOption("filterDATE")){
+                    String[] parts = filterDATE.split("_");
+                    String dateFrom = parts[0];
+                    String dateTo = parts[1];
+                    set = filter.filterByDate(set,dateFrom,dateTo);
+                }
                 raport.generateRaport(set);
                 System.out.println("");
                 printReport(raport,line);
@@ -37,6 +62,15 @@ public class MainJarAndCommandLineTEST {
             if(line.hasOption("report2")) {
                 Set<Project> set = importer.importDataFromFiles(path);
                 raport = new Raport2();
+                if(line.hasOption("filterEMP")){
+                    set = filter.filterByEmployeeName(set,filterEMP);
+                }
+                if(line.hasOption("filterDATE")){
+                    String[] parts = filterDATE.split("_");
+                    String dateFrom = parts[0];
+                    String dateTo = parts[1];
+                    set = filter.filterByDate(set,dateFrom,dateTo);
+                }
                 raport.generateRaport(set);
                 System.out.println("");
                 printReport(raport,line);
@@ -44,6 +78,15 @@ public class MainJarAndCommandLineTEST {
             if(line.hasOption("report3")) {
                 Set<Project> set = importer.importDataFromFiles(path);
                 raport = new Raport3();
+                if(line.hasOption("filterEMP")){
+                    set = filter.filterByEmployeeName(set,filterEMP);
+                }
+                if(line.hasOption("filterDATE")){
+                    String[] parts = filterDATE.split("_");
+                    String dateFrom = parts[0];
+                    String dateTo = parts[1];
+                    set = filter.filterByDate(set,dateFrom,dateTo);
+                }
                 raport.generateRaport(set);
                 System.out.println("");
                 printReport(raport,line);
@@ -51,6 +94,15 @@ public class MainJarAndCommandLineTEST {
             if(line.hasOption("report4")) {
                 Set<Project> set = importer.importDataFromFiles(path);
                 raport = new Raport4();
+                if(line.hasOption("filterEMP")){
+                    set = filter.filterByEmployeeName(set,filterEMP);
+                }
+                if(line.hasOption("filterDATE")){
+                    String[] parts = filterDATE.split("_");
+                    String dateFrom = parts[0];
+                    String dateTo = parts[1];
+                    set = filter.filterByDate(set,dateFrom,dateTo);
+                }
                 raport.generateRaport(set);
                 System.out.println("");
                 printReport(raport,line);
@@ -62,15 +114,9 @@ public class MainJarAndCommandLineTEST {
             if(line.hasOption("info")) {
                 System.out.println("operations manual.....");
             }
-            if(line.hasOption("failures")) {
-                System.out.println("running......");
-            }
-            if(line.hasOption("range_from")) {
-                System.out.println("running......");
-            }
-            if(line.hasOption("range_to")) {
-                System.out.println("running......");
-            }
+//            if(line.hasOption("filterEMP")) {
+//                System.out.println(filterEMP);
+//            }
         }
         catch( ParseException exp ) {
             System.err.println( "Parsing failed.  Reason: " + exp.getMessage() );
@@ -119,6 +165,20 @@ public class MainJarAndCommandLineTEST {
                 e.printStackTrace();
             }
         }
+        if(line.hasOption("chart")) {
+            //System.out.println("saving......as PDF into project directory");
+            printer = new ChartDataRaportPrinter();
+            try {
+                printer.printRaport(raport);
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
     }
 
 }
